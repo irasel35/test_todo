18b8af6873b3160f22b4c0bd223905387fbb448e
irasel35 <raselcse68@gmail.com>
2021-06-06 16:37:56 +0600

    add new todo to list

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
     <Header />
-<Todos v-bind:todos="todos" :xyz="msg"  
+    <AddTodo v-on:add-todo="addTodo"/>
+  <Todos v-bind:todos="todos" :xyz="msg"  
             v-on:del-todo="deleteTodo"/>
 
  </div>
 </template>
 
 <script>
+import AddTodo from './components/AddTodo';
 import Header from './components/layout/header';
 import Todos from './components/Todos';
 
@@ -15,7 +17,8 @@ export default {
   name: 'App',
   components: {
     Todos,
-    Header
+    Header,
+    AddTodo
 
   },
   data(){
@@ -25,13 +28,13 @@ export default {
       {
         id:1,
         title:"Todo One",
-        completed:true
+        completed:false
          
       },
        {
         id:2,
         title:"Todo Two",
-        completed:true
+        completed:false
          
       },
        {
@@ -47,11 +50,24 @@ export default {
     deleteTodo(id){
 this.todos=this.todos.filter(todo=>todo.id !== id)
 
+  },
+  addTodo(newTodo){
+    this.todos=[...this.todos, newTodo]; 
+    //adding new member in array
   }
   }
 }
 </script>
 
 <style>
+.sbmt-btn{
+  display: inline;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+  /* text-align: left; */
+}
 
 </style>
