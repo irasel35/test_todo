39dea439399b7ed9fd54218d541cace8feba4936
irasel35 <raselcse68@gmail.com>
2021-06-06 16:57:08 +0600

    https request added

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
 import AddTodo from './components/AddTodo';
 import Header from './components/layout/header';
 import Todos from './components/Todos';
+import axios from'axios';
 
 export default {
   name: 'App',
@@ -55,6 +56,12 @@ this.todos=this.todos.filter(todo=>todo.id !== id)
     this.todos=[...this.todos, newTodo]; 
     //adding new member in array
   }
+  },
+  // https request created with axios
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=4')
+    .then(res=>this.todos=res.data)
+    .catch(err => console.log(err));
   }
 }
 </script>
